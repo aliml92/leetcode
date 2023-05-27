49ef6503a1dc45bf799af0bdec6c4458efb132d3
aliml92 <almowins5yearsss@gmail.com>
2023-05-27 21:50:09 +0900

    a leetcode problem

---
+++ array_hashing/0554_brick_wall/solution.go
@@ -0,0 +1,23 @@
+package solution
+
+func leastBricks(wall [][]int) int {
+	m := make(map[int]int,0)
+	for _, layer := range wall {
+		pos := 0
+		brickNums := len(layer) 
+		for i, brick := range layer {
+			if i != brickNums-1 { 
+				pos = pos + brick
+				m[pos]++
+			}
+		}
+	}
+	height := len(wall)
+	h := 0
+	for _, v := range m {
+		if v > h {
+			h = v
+		}
+	}
+	return height-h
+}
---
+++ array_hashing/0554_brick_wall/solution_test.go
@@ -0,0 +1,25 @@
+package solution
+
+import "testing"
+
+func Test_leastBricks(t *testing.T) {
+	type args struct {
+		wall [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"example1", args{[][]int{{1,2,2,1},{3,1,2},{1,3,2},{2,4},{3,1,2},{1,3,1,1}}}, 2},
+		{"example2", args{[][]int{{1},{1},{1}}}, 3},
+		{"example3", args{[][]int{{1,1,1,1,1,1,1,1,1,1},{1,1,1,1,1,1,1,1,1,1},{1,1,1,1,1,1,1,1,1,1}}}, 0},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := leastBricks(tt.args.wall); got != tt.want {
+				t.Errorf("leastBricks() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
