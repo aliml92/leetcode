15aad132baf4da04a0513c9b070ccbd391167669
aliml92 <almowins5yearsss@gmail.com>
2024-02-10 10:49:33 +0500

    add alternative soltion

--- sql/1934_confirmation_rate/solution.sql
+++ sql/1934_confirmation_rate/solution.sql
@@ -1,8 +1,19 @@
+-- first version
 SELECT 
     user_id, 
     ROUND((count(user_id) FILTER (WHERE action = 'confirmed')) / (count(user_id) FILTER (WHERE action IS NOT NULL))::decimal, 2)  AS confirmation_rate  
 FROM (
     SELECT s.user_id, COALESCE(c.action, 'timeout') as action   
+    FROM Signups s
+    LEFT JOIN Confirmations c ON c.user_id = s.user_id
+) GROUP BY user_id
+
+
+-- second version
+SELECT 
+    user_id, 
+    ROUND((count(user_id) FILTER (WHERE action = 'confirmed'))/(count(user_id))::decimal, 2)  AS confirmation_rate  FROM (
+    SELECT s.user_id, c.action
     FROM Signups s
     LEFT JOIN Confirmations c ON c.user_id = s.user_id
 ) GROUP BY user_id
